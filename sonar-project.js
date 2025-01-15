32b2bdac0bcfb1da65d4e92f99630e16f67c72f0
CEO <58459770+aviationlibrary@users.noreply.github.com>
2025-01-15 12:07:16 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.91.78.168:9000/',
+  serverUrl: 'http://sonarqube:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': 'cc1f0546028f17c06a8e576637e78d6b983206f0',
-	    //'sonar.password': 'admin',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'ROMANdwhizz001$',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
